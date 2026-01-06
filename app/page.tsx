dc053200fe44d7e0cefd3b5fec547659217fcdaa
Andrew Williams <32201177+asw4321@users.noreply.github.com>
2026-01-06 10:01:02 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center px-6">
+      <div class="container">
+          <img src="logo/integry_png" alt="Centered Image">
+      </div>
       <div className="max-w-2xl text-center">
 
         <p className="mt-4 text-lg text-gray-600">
